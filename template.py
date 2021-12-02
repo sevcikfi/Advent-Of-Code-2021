afd5bfa24f5af53578cb5ce7aadd2828a1bf1aab
“Filip” <fisevcik@email.cz>
2021-12-02 04:23:27 +0100

    create template.py

---
+++ template.py
@@ -0,0 +1,20 @@
+
+file = "input.txt"
+input = []
+with open(file, 'r') as file:
+    input = file.readlines()
+
+
+def solve1():
+    print("solution 1")
+
+def solve2():
+    print("solution 2")
+
+
+def main():
+    solve1()
+    #solve2();
+
+
+main()
