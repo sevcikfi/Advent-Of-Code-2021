14e52630bfd25ca6e8e3f0f14caf5bf3b59d77cf
“Filip” <fisevcik@email.cz>
2021-12-02 05:22:17 +0100

    Day 01: Solve second problem

--- 01/solution.py
+++ 01/solution.py
@@ -1,11 +1,11 @@
 
 path = r"C:\Github\Advent-Of-Code\01\input.txt"
 #path = r".\input.txt"
-input = []
+input = 0
 with open(path, 'r') as file:
     raw = file.readlines()
     input = [int(x) for x in raw ]
-
+    input
 def solve1():
     print("solution 1")
     #print(input)
@@ -20,7 +20,16 @@ def solve1():
 def solve2():
     print("solution 2")
     
+    incerases = 0
+    before = input[0] + input[1] + input[2]
+    next = 0
 
+    for i in range(0, len(input)-2):
+        next = input[i] + input[i+1] + input[i+2]
+        if before < next:
+            incerases = incerases + 1
+        before = next
+    print(incerases)
 
 def main():
     solve1()
