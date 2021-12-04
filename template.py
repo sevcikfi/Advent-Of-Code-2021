bf6938459f1b7cd326331e60743cbc985098f30a
“Filip” <fisevcik@email.cz>
2021-12-04 09:28:42 +0100

    Update template.py

--- template.py
+++ template.py
@@ -1,6 +1,7 @@
 path = r"C:\Github\Advent-Of-Code\<Number>\input.txt" #conda things
 with open(path, 'r') as file:
-    input = file.readlines()
+    raw = file.readlines()
+#input = [list(map(int, (line.strip()))) for line in raw] #Read comp for number
 
 def solve1():
     return 1    
@@ -11,5 +12,6 @@ def solve2():
 def main():
     print("solution 1: {}".format(solve1()))
     print("solution 2: {}".format(solve2()))
+    
 if __name__ == "__main__":
     main()
