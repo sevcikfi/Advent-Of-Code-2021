e1886ed40eaebcf708ebc3569579b158eef6d3a5
“Filip” <fisevcik@email.cz>
2021-12-10 22:31:21 +0100

    Add outline for Day 04

---
+++ 04/solution.py
@@ -0,0 +1,17 @@
+path = r"C:\Github\Advent-Of-Code\04\input.txt" #conda things
+with open(path, 'r') as file:
+    raw = file.readlines()
+#input = [list(map(int, (line.strip()))) for line in raw] #Read comp for number
+
+def solve1():
+    return 1    
+
+def solve2():
+    return 2
+
+def main():
+    print("solution 1: {}".format(solve1()))
+    print("solution 2: {}".format(solve2()))
+    
+if __name__ == "__main__":
+    main()
